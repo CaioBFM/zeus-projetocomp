d4666a1db17f259e4974f6520b5ff50d1cabd54c
CaioBFM <caiofinocchio@outlook.com>
2025-05-18 23:19:49 -0300

    adicionando mais membros e orçamentos

--- components/MembersContext.tsx
+++ components/MembersContext.tsx
@@ -23,32 +23,58 @@ interface MembersContextType {
 const MembersContext = createContext<MembersContextType | undefined>(undefined);
 
 export function MembersProvider({ children }: { children: ReactNode }) {
-  const [membros, setMembros] = useState<Member[]>([
-    {
-      id: '1',
-      nome: 'Lady Gaga',
-      dataNascimento: '01/01/1990',
-      email: 'gaga@zeus.com',
-      cargo: 'Analista',
-      telefone: '11999999999',
-      genero: 'Feminino',
-      dataIngresso: '01/01/2022',
-      habilidades: 'Excel, Comunicação',
-      imagem: 'https://snworksceo.imgix.net/tdl/542c7960-10ed-4dd3-bfda-8c4c963ecda6.sized-1000x1000.png?w=1000',
+  const [membros, setMembros] = useState<Member[]>(
+    [
+      {
+        id: '1',
+        nome: 'Lady Gaga',
+        dataNascimento: '01/01/1990',
+        email: 'gaga@zeus.com',
+        cargo: 'Analista',
+        telefone: '11999999999',
+        genero: 'Feminino',
+        dataIngresso: '01/01/2022',
+        habilidades: 'Excel, Comunicação',
+        imagem: 'https://snworksceo.imgix.net/tdl/542c7960-10ed-4dd3-bfda-8c4c963ecda6.sized-1000x1000.png?w=1000',
       },
-    {
-      id: '2',
-      nome: 'Satoru Gojo',
-      dataNascimento: '15/05/1985',
-      email: 'satoru@zeus.com',
-      cargo: 'Desenvolvedor',
-      telefone: '11988888888',
-      genero: 'Masculino',
-      dataIngresso: '10/03/2021',
-      habilidades: 'React, Node.js',
-      imagem: 'https://cdn.pfps.gg/pfps/5051-gojo-manga.png',
-    },
-  ]);
+      {
+        id: '2',
+        nome: 'Satoru Gojo',
+        dataNascimento: '15/05/1985',
+        email: 'satoru@zeus.com',
+        cargo: 'Desenvolvedor',
+        telefone: '11988888888',
+        genero: 'Masculino',
+        dataIngresso: '10/03/2021',
+        habilidades: 'React, Node.js',
+        imagem: 'https://cdn.pfps.gg/pfps/5051-gojo-manga.png',
+      },
+      {
+        id: '3',
+        nome: 'Batman',
+        dataNascimento: '01/02/1975',
+        email: 'batman@wayne.com',
+        cargo: 'Detetive',
+        telefone: '11977777777',
+        genero: 'Masculino',
+        dataIngresso: '01/01/2020',
+        habilidades: 'Investigação, Estratégia',
+        imagem: 'https://preview.redd.it/batman-profile-pics-v0-qrz10k9s6fid1.jpg?width=542&format=pjpg&auto=webp&s=2f848b2d6644cbb03e0116e28755cd7e0a63319a',
+      },
+      {
+        id: '4',
+        nome: 'Thomas Shelby',
+        dataNascimento: '10/06/1890',
+        email: 'thomas.shelby@peakyblinders.com',
+        cargo: 'Líder',
+        telefone: '11966666666',
+        genero: 'Masculino',
+        dataIngresso: '01/01/2019',
+        habilidades: 'Liderança, Negociação',
+        imagem: 'https://rollingstone.com.br/media/uploads/ciliian_murphy.jpg',
+      },
+    ]
+  );
 
   const addMember = (m: Member) => setMembros(prev => [...prev, m]);
   const removeMember = (id: string) => setMembros(prev => prev.filter(m => m.id !== id));
